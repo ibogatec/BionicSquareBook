9986dd88c52132811003f468fa772cd7f6548bd1
Ivan Bogatec <ivan.bogatec@gmail.com>
2023-08-13 15:28:34 +0200

    BionicSquareWeb: Empty ASP.NET Web project

---
+++ BionicSquareWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BionicSquareWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
