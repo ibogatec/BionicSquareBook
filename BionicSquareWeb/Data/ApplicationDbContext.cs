e0b8ff9bef8a2ebb45b7a3e63cdb47468e187dc9
Ivan Bogatec <ivan.bogatec@gmail.com>
2023-10-20 17:29:22 +0200

    Add migration and initial seed data: SeedCategoryTable

--- BionicSquareWeb/Data/ApplicationDbContext.cs
+++ BionicSquareWeb/Data/ApplicationDbContext.cs
@@ -8,4 +8,16 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     
     public DbSet<Category> Categories { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Action" },
+            new Category { Id = 2, Name = "SciFi" },
+            new Category { Id = 3, Name = "History" }
+        );
+        
+    }
 }
