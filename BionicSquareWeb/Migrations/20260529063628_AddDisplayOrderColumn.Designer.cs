55703aeaab53ce9420487d29cb09229e4e586301
Ivan Bogatec <ivan.bogatec@gmail.com>
2023-10-30 17:42:43 +0200

    Add DisplayOrder column in Category table

--- BionicSquareWeb/Migrations/20260529063628_AddDisplayOrderColumn.Designer.cs
+++ BionicSquareWeb/Migrations/20260529063628_AddDisplayOrderColumn.Designer.cs
@@ -3,6 +3,7 @@ using BionicSquareWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BionicSquareWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260529063628_AddDisplayOrderColumn")]
+    partial class AddDisplayOrderColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -44,19 +47,19 @@ namespace BionicSquareWeb.Migrations
                         new
                         {
                             Id = 1,
-                            DisplayOrder = 1,
+                            DisplayOrder = 0,
                             Name = "Action"
                         },
                         new
                         {
                             Id = 2,
-                            DisplayOrder = 2,
+                            DisplayOrder = 0,
                             Name = "SciFi"
                         },
                         new
                         {
                             Id = 3,
-                            DisplayOrder = 3,
+                            DisplayOrder = 0,
                             Name = "History"
                         });
                 });
--- BionicSquareWeb/Migrations/20260529064046_SeedDisplayOrderColumn.Designer.cs
+++ BionicSquareWeb/Migrations/20260529064046_SeedDisplayOrderColumn.Designer.cs
@@ -3,6 +3,7 @@ using BionicSquareWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BionicSquareWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260529064046_SeedDisplayOrderColumn")]
+    partial class SeedDisplayOrderColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
