4b27f8c4c83dcf8f5d088f041bcf4e00e7f2c138
Ivan Bogatec <ivan.bogatec@gmail.com>
2023-11-06 17:38:16 +0200

    Update Views and CategoryController

--- BionicSquareWeb/Controllers/CategoryController.cs
+++ BionicSquareWeb/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
+using BionicSquareWeb.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BionicSquareWeb.Controllers;
 
 public class CategoryController : Controller
 {
+    private readonly ApplicationDbContext _context;
+    
+    public CategoryController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
     public IActionResult Index()
     {
-        return View();
+        var categories = _context.Categories.ToList();
+        return View("Index", categories);
     }
     
 }
