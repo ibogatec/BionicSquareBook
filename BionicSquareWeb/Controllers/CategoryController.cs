d9477cf7533732b530593e2a51b0c8c78069ba83
Ivan Bogatec <ivan.bogatec@gmail.com>
2023-12-13 17:10:14 +0200

    Add Update for Category endpoint

--- BionicSquareWeb/Controllers/CategoryController.cs
+++ BionicSquareWeb/Controllers/CategoryController.cs
@@ -46,4 +46,43 @@ public class CategoryController : Controller
         return RedirectToAction("Index", "Category");
     }
     
+    public IActionResult Update(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        
+        var category = _context.Categories.Find(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        
+        return View(category);
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [ActionName("Update")]
+    public IActionResult UpdatePost(Category category)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+        
+        bool isDuplicate = _context.Categories.Any(c => c.Name.ToLower() == category.Name.ToLower() && c.Id != category.Id);
+        if (isDuplicate)
+        {
+            ModelState.AddModelError("Name", $"Category with the name '{category.Name}' already exists");
+            return View();
+        }
+        
+        _context.Categories.Update(category);
+        _context.SaveChanges();
+        return RedirectToAction("Index", "Category");
+    }
+    
+
 }
