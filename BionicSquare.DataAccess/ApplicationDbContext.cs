b224399e11739c88f6ada79d1c2e97f5113a9a53
Ivan Bogatec <ivan.bogatec@gmail.com>
2024-08-24 17:48:34 +0200

    Add Product model

--- BionicSquare.DataAccess/ApplicationDbContext.cs
+++ BionicSquare.DataAccess/ApplicationDbContext.cs
@@ -9,6 +9,8 @@ public class ApplicationDbContext : DbContext
     
     public DbSet<Category> Categories { get; set; }
     
+    public DbSet<Product> Products { get; set; }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         // base.OnModelCreating(modelBuilder);
---
+++ BionicSquare.Models/Product.cs
@@ -0,0 +1,46 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace BionicSquare.Models;
+
+public class Product
+{
+    [Key]
+    public int Id { get; init; }
+    
+    [Required]
+    public string Title { get; init; } = string.Empty;
+    
+    public string Description { get; init; } = string.Empty;
+    
+    [Required]
+    public string Isbn { get; init; } = string.Empty;
+    
+    [Required]
+    public string Author { get; init; } = string.Empty;
+    
+    [Required]
+    [Range(1, 1000, ErrorMessage = "Price must be between 1 and 1000")]
+    [Display(Name = "List Price")]
+    public double ListPrice { get; init; }
+    
+    [Required]
+    [Range(1, 1000, ErrorMessage = "Price must be between 1 and 1000")]
+    [Display(Name = "Price for 1-50")]
+    public double Price { get; init; }
+    
+    [Required]
+    [Range(1, 1000, ErrorMessage = "Price must be between 1 and 1000")]
+    [Display(Name = "Price for 50+")]
+    public double Price50 { get; init; }
+    
+    [Required]
+    [Range(1, 1000, ErrorMessage = "Price must be between 1 and 1000")]
+    [Display(Name = "Price for 100+")]
+    public double Price100 { get; init; }
+    
+    [ValidateNever]
+    [Display(Name = "Product Image")]
+    public string? ImageUrl { get; init; }
+    
+}
