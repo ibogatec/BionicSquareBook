455c9d244bf12faa405fba9a9afb11a8bc2d2d23
Ivan Bogatec <ivan.bogatec@gmail.com>
2024-08-25 19:14:58 +0200

    Add migration to seed initial Products

--- BionicSquare.DataAccess/Migrations/20260603090207_ModifyProductTable_SeedInitialProducts.Designer.cs
+++ BionicSquare.DataAccess/Migrations/20260603090207_ModifyProductTable_SeedInitialProducts.Designer.cs
@@ -3,6 +3,7 @@ using BionicSquare.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BionicSquare.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260603090207_ModifyProductTable_SeedInitialProducts")]
+    partial class ModifyProductTable_SeedInitialProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
