cbccce4d951c00ad7ff43378ad8ba42a7414a2ea
Ivan Bogatec <ivan.bogatec@gmail.com>
2026-04-27 15:09:29 +0200

    Add register user and logout functionality

--- BionicSquare.Web/Areas/Identity/Controllers/AccountController.cs
+++ BionicSquare.Web/Areas/Identity/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using BionicSquare.Models;
 using BionicSquare.Models.ViewModels;
 
 namespace BionicSquareWeb.Areas.Identity.Controllers;
@@ -6,6 +8,17 @@ namespace BionicSquareWeb.Areas.Identity.Controllers;
 [Area("Identity")]
 public class AccountController : Controller
 {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    
+    public AccountController(
+        UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
     [HttpGet]
     [ActionName("Login")]
     public IActionResult LoginGet()
@@ -14,6 +27,7 @@ public class AccountController : Controller
     }
 
     [HttpPost]
+    [ValidateAntiForgeryToken]
     [ActionName("Login")]
     public IActionResult LoginPost(LoginViewModel loginViewModel)
     {
@@ -33,6 +47,15 @@ public class AccountController : Controller
         return View();
     }
     
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [ActionName("Logout")]
+    public async Task<IActionResult> LogoutPost()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home",  new { area = "Customer" });
+    }
+    
     [HttpGet]
     [ActionName("Register")]
     public IActionResult Register()
@@ -41,23 +64,48 @@ public class AccountController : Controller
     }
 
     [HttpPost]
+    [ValidateAntiForgeryToken]
     [ActionName("Register")]
-    public IActionResult RegisterPost(RegisterViewModel registerViewModel)
+    public async Task<IActionResult> RegisterPostAsync(RegisterViewModel registerViewModel)
     {
         try
         {
             if (!ModelState.IsValid)
             {
-                return View();
+                return View(registerViewModel);
+            }
+            
+            ApplicationUser appUser = new()
+            {
+                UserName = registerViewModel.Name,
+                Email = registerViewModel.Email,
+                PhoneNumber = registerViewModel.PhoneNumber,
+                Name = registerViewModel.Name,
+                PostalCode = registerViewModel.PostalCode,
+                State = registerViewModel.State,
+                StreetAddress = registerViewModel.StreetAddress,
+                City = registerViewModel.City
+            };
+            
+            var result = await _userManager.CreateAsync(appUser, registerViewModel.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(appUser, isPersistent: false);
+                return RedirectToAction("Index", "Home",  new { area = "Customer" });
             }
 
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+            
+            return View(registerViewModel);
         }
         catch (Exception e)
         {
-
+            ModelState.AddModelError("", $"Error: {e.Message}");
+            return View(registerViewModel);
         }
-        
-        return View();
     }
 
     [HttpGet]
